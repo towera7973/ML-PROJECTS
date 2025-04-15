f726c7cc380e046b39c64032e09edd088562e286
towera7973 <56852117+towera7973@users.noreply.github.com>
2025-04-15 17:29:06 +0000

    commit

--- suppervector/supervector.py
+++ suppervector/supervector.py
@@ -8,6 +8,8 @@ print(Iris_data.feature_names)
 #reading data with its feature names the convinient way with pandas
 Iris_data=pd.DataFrame(Iris_data.data, columns=Iris_data.feature_names)
 print(Iris_data.head())
+#show the target aswel (the digits of the flowers ,e.g 0.1.2
+
 
 
 
